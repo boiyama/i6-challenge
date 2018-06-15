53ecbb5159f04ff19cde675617d9b6fa83da2abb
boiyaa <boiyaa@hotmail.com>
2018-06-15 10:24:44 +0900

    Update pages

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
+import Head from "next/head";
 import Link from "next/link";
 
-const Index = () => (
-  <div>
+const style = {
+  container: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    fontSize: "10rem"
+  }
+};
+
+export default () => (
+  <div style={Object.assign({ backgroundColor: "yellow" }, style.container)}>
+    <Head>
+      <style>html, body, #__next {"{height:100%;margin:0;}"}</style>
+    </Head>
+    <span style={style.text}>Home</span>
     <Link href="/about">
-      <a>About Page</a>
+      <a style={style.text}>About</a>
     </Link>
-    <p>Hello Next.js</p>
   </div>
 );
-
-export default Index;
